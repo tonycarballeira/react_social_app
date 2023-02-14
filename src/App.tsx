dcc6a67152b9eff18f8f63f94550f50474755d48
tonycarballeira <tonycarballeira@gmail.com>
2023-02-14 12:36:55 -0500

    created a navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
+
+
 function App() {
   return (
     <div className="App">
       <Router>
+
+        <Navbar />
+
         <Routes>
           <Route path="/" element={ <Main /> }></Route>
+          <Route path="/login" element={ <Login /> }></Route>
         </Routes>
       </Router>
     </div>
