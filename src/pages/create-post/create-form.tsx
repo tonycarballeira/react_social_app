cb968843807c80fd71f33dab58758ca0bb517796
tonycarballeira <tonycarballeira@gmail.com>
2023-02-14 14:40:27 -0500

    added error messages for empty form field submission

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -2,25 +2,32 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup"; 
 import { yupResolver } from "@hookform/resolvers/yup";
 
+interface CreateFormData {
+    title: string;
+    description: string;
+}
+
 export const CreateForm = () => {
     const schema = yup.object().shape({
         title: yup.string().required("You must add a title."),
-        description: yup.string().required(),
+        description: yup.string().required("You must add a description."),
 
     });
 
-    const {  register, handleSubmit } = useForm({
+    const {  register, handleSubmit, formState: {errors} } = useForm<CreateFormData>({
         resolver: yupResolver(schema),
     });
 
-    const onCreatePost = (data:any) => {
+    const onCreatePost = (data: CreateFormData) => {
         console.log(data);
     }
 
     return (
         <form onSubmit={handleSubmit(onCreatePost)}>
             <input placeholder="Title..." {...register("title")}></input>
+            <p style={{color: "red"}}>{errors.title?.message}</p>
             <textarea placeholder="Description..." {...register("description")}></textarea>
+            <p style={{color: "red"}}>{errors.description?.message}</p>
             <input type="submit"></input>
         </form>
     )
