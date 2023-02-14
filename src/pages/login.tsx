5f3a7148bc18ba978c3ab87a666d30fbf63da5df
tonycarballeira <tonycarballeira@gmail.com>
2023-02-14 13:12:18 -0500

    added google user auth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,20 @@
+import { auth, provider } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
-    return <div> Login Page </div>;
+    const navigate = useNavigate();
+
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+        navigate('/');
+    }
+
+    return (
+        <div> 
+            <p> Sign in with google to continue</p> 
+            <button onClick={signInWithGoogle}> Sign in with Google </button>
+        </div>
+    )
 }
