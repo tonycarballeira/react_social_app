5525cff1f64ab80ae2dc89e2aa45d3e9eb5062a3
tonycarballeira <tonycarballeira@gmail.com>
2023-02-14 12:25:55 -0500

    created route for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Main } from "./pages/main";
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Main /> }></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
